430e479988a2654dec55e3fcfe1107c1a01f4c04
Vladislav <komarov_v_y@ngs.ru>
2021-03-14 12:51:29 +0700

    Add class Cat(Pet)

--- pet_classes.py
+++ pet_classes.py
@@ -9,6 +9,17 @@ class Pet:
         print(f"{self.name}: {self.kind}, {self.age} years old, {self.sex}")
 
 
+class Cat(Pet):
+    kind = 'cat'
+
+    def __init__(self, coat_length, age, sex, name):
+        self.coat_len = coat_length
+        self.age = age
+        self.sex = sex
+        self.name = name
+
 if __name__ == '__main__':
-    test_pet = Pet(kind='dog', age=5, sex='male', name='Rex')
+    test_pet = Pet(kind='dog', age=5, sex='female', name='Rex')
     test_pet.info()
+    test_cat = Cat(coat_length='long', age=2, sex='male', name='Sam')
+    test_cat.info()
